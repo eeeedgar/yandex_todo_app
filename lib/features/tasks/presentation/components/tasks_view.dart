3d7bf0de18479e626ea4fcb6ce5e7728c8a14368
eeeedgar <wocoru@gmail.com>
2024-06-23 06:16:49 +0300

    feat: add tile "create new task"

--- lib/features/tasks/presentation/components/tasks_view.dart
+++ lib/features/tasks/presentation/components/tasks_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:yandex_todo_app/app/presentation/app_fonts.dart';
 import 'package:yandex_todo_app/app/presentation/custom_app_colors.dart';
+import 'package:yandex_todo_app/features/task_edit/presentation/task_edit_screen.dart';
 import 'package:yandex_todo_app/features/tasks/domain/entity/complete_status_enum.dart';
 import 'package:yandex_todo_app/features/tasks/domain/state/tasks_cubit.dart';
 import 'package:yandex_todo_app/features/tasks/presentation/components/task_dismissable_tile.dart';
@@ -46,15 +48,37 @@ class TasksView extends StatelessWidget {
               ),
               sliver: SliverPadding(
                 padding: const EdgeInsets.symmetric(vertical: 8),
-                sliver: SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                    childCount: tasks.length,
-                    (_, index) {
-                      final task = tasks[index];
-                      return TaskDismissableTile(task: task);
-                    },
+                sliver: SliverMainAxisGroup(slivers: [
+                  SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      childCount: tasks.length,
+                      (_, index) {
+                        final task = tasks[index];
+                        return TaskDismissableTile(task: task);
+                      },
+                    ),
                   ),
-                ),
+                  SliverToBoxAdapter(
+                    child: ListTile(
+                      onTap: () => Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => const TaskEditScreen(),
+                        ),
+                      ),
+                      title: Padding(
+                        padding: const EdgeInsets.only(left: 64),
+                        child: Text(
+                          'Новое',
+                          style: AppFonts.b1.copyWith(
+                            color: Theme.of(context)
+                                .extension<CustomAppColors>()!
+                                .labelTertiary,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ]),
               ),
             );
           },
